db3e05f0e2fc9bcec45131ce5da6f02d8b7c3456
Grzegorz Git <grzegorz.chabiera@gmail.com>
2023-09-26 20:25:48 +0200

    Security

--- storage-service/src/main/java/com/epam/storageservice/StorageServiceApplication.java
+++ storage-service/src/main/java/com/epam/storageservice/StorageServiceApplication.java
@@ -8,10 +8,25 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @RequiredArgsConstructor
+@EnableWebSecurity
+@EnableMethodSecurity
 public class StorageServiceApplication implements CommandLineRunner {
 
     private final StorageRepository storageRepository;
@@ -28,4 +43,33 @@ public class StorageServiceApplication implements CommandLineRunner {
         storageRepository.save(storage2);
     }
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.GET, "/storages").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/storages").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/storages").hasRole("ADMIN")
+                )
+                .oauth2ResourceServer(resourceServer -> resourceServer
+                        .jwt(jwtConfigurer -> jwtConfigurer
+                                .jwtAuthenticationConverter(jwtAuthenticationConverter())))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+
+    @Bean
+    @SuppressWarnings("unchecked")
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            List<String> roles = (List<String>) jwt.getClaimAsMap("realm_access").get("roles");
+            return roles.stream()
+                    .map(roleName -> "ROLE_" + roleName)
+                    .map(SimpleGrantedAuthority::new)
+                    .collect(Collectors.toList());
+        });
+        return converter;
+    }
 }
